eda10dbb0acd7959db70c62ebedff3f0d1005a74
AlexChuSmartPrototyping <41936217+AlexChuSmartPrototyping@users.noreply.github.com>
2020-05-29 13:56:54 +0800

    Update main.py

--- micropython/main.py
+++ micropython/main.py
@@ -1,3 +1,13 @@
+"""
+Demo code for RFD77402 TOF sensor
+Auther: Alex Chu
+Website: www.smart-prototyping.com
+Email: shop@smart-prototyping.com
+TOF RFD77402 product link: https://www.smart-prototyping.com/Zio-TOF-Distance-Sensor-RFD77402.html
+Display product link: https://www.smart-prototyping.com/Zio-OLED-Display-0-91-in-128-32-Qwiic.html
+Library: https://github.com/ZIOCC/Qwiic_TOF_Module_RFD77402/tree/master/micropython
+"""
+
 from machine import Pin,I2C
 import rfd77402
 from ssd1306 import SSD1306_I2C
